570885dc2f81d5a641b7c91a4d3444149295da80
KEREM <keremcaan@hotmail.com>
2022-05-02 20:13:12 +0300

    Initial commit

---
+++ src/Basic Calculator.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+val x = Scanner(System.`in`)
+    println("Toplama (1)")
+    println("Çıkarma (2)")
+    println("Çarpma (3)")
+    println("Bölme (4)")
+
+    var işlem = x.nextInt()
+    println("Birinci sayıyı girin: ")
+    var sayi1 = x.nextInt()
+    println("İkinci sayıyı girin: ")
+    var sayi2 = x.nextInt()
+    when (işlem) {
+        1 -> println("Sonuç: ${sayi1+sayi2}")
+        2 -> println("Sonuç: ${sayi1-sayi2}")
+        3 -> println("Sonuç: ${sayi1*sayi2}")
+        4 -> println("Sonuç: ${sayi1/sayi2}")
+    }
+}
